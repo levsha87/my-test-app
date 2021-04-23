74f77d1baae8638dd43936469be289368c798af2
Roma Levsha <rm.levsha@gmail.com>
2021-04-23 09:33:48 +0300

    feat: add method map to MyPosts

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,21 +3,35 @@ import s from './Dialogs.module.css';
 import Dialog from './Dialog/Dialog';
 import Message from './Message/Message';
 
+let dialogsData = [
+  { id: 1, name: 'Andrei' },
+  { id: 2, name: 'Yurii' },
+  { id: 3, name: 'Vadim' },
+  { id: 4, name: 'Marina' },
+  { id: 5, name: 'Denis' },
+  { id: 6, name: 'Grigory' },
+];
+
+let messageData = [
+  { id: 1, message: 'Hello' },
+  { id: 2, message: 'How are you?' },
+  { id: 3, message: 'I am fine!' },
+  { id: 4, message: 'I am glad to see you' },
+  { id: 5, message: 'Are you ready?' },
+  { id: 6, message: 'Go!!' },
+];
+
+let dialogsElements = dialogsData.map((elem) => <Dialog name={elem.name} adress={elem.id} />);
+let messageElements = messageData.map((elem) => <Message message={elem.message} />);
+
 const Dialogs = (props) => {
   return (
     <section className={s.dialogs}>
       <section className={s.dialogsItems}>
-        <Dialog name='Andrei' adress='1' />
-        <Dialog name='Yurii' adress='2' />
-        <Dialog name='Vadim' adress='3' />
-        <Dialog name='Marina' adress='4' />
-        <Dialog name='Denis' adress='5' />
-        <Dialog name='Grigory' adress='6' />
+        {dialogsElements}
       </section>
       <section className={s.messages}>
-        <Message message='Hello' />
-        <Message message='How are you?' />
-        <Message message='I am fine!' />
+        {messageElements}
       </section>
     </section>
   );
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,16 @@ import { React } from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
+let postData = [
+  { id: 1, message: 'Hi, how are you?', likeNumber: '15' },
+  { id: 1, message: 'Hello, I am fine!', likeNumber: '25' },
+  { id: 1, message: 'Hello, I am good!', likeNumber: '35' },
+];
+
+let postElements = postData.map((elem) => (
+  <Post message={elem.message} likeNumber={elem.likeNumber} />
+));
+
 const MyPosts = () => {
   return (
     <div>
@@ -14,11 +24,7 @@ const MyPosts = () => {
           <button>Add post</button>
         </div>
       </div>
-      <div className={s.posts}>
-        <Post message='Hi, how are you?' likeNumber='15' />
-        <Post message='Hello, I am fine!' likeNumber='25' />
-        <Post message='Hello, I am good!' likeNumber='35' />
-      </div>
+      <div className={s.posts}>{ postElements }</div>
     </div>
   );
 };
